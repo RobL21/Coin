992e4bc3f34fbd750070b27940771783f805ca39
Robert <robertl193@nycstudents.net>
2026-09-22 15:28:47 -0400

    Flipped a coin

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        Coin penny = new Coin();
+        System.out.println(penny);
+        System.out.println(penny.getState());
+        penny.flip();
+        System.out.println(penny.getState());
+    }
+}

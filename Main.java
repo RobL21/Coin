ac8b11805683d5202b248f5c8ca65f0228183ac5
Robert <robertl193@nycstudents.net>
2026-09-23 15:20:24 -0400

    Finished Player

--- Main.java
+++ Main.java
@@ -1,9 +1,22 @@
 public class Main {
     public static void main(String[] args){
         Coin penny = new Coin();
-        System.out.println(penny);
-        System.out.println(penny.getState());
-        penny.flip();
-        System.out.println(penny.getState());
+        //System.out.println(penny);
+        //System.out.println(penny.getState());
+        //penny.flip();
+        //System.out.println(penny.getState());
+        //System.out.println(penny.getHeads());
+        //System.out.println(penny.getTails());
+        //Coin nickel = new Coin(0.9);
+        //nickel.flip(100);
+        //System.out.println(nickel.getHeads());
+        //System.out.println(nickel.getTails());
+        //nickel.setPTails(0.5);
+        //nickel.flip(100);
+        //System.out.println(nickel.getHeads());
+        //System.out.println(nickel.getTails());
+        Player rob = new Player(100);
+        rob.flip(penny, "tails", 50);
+        System.out.println(rob.getBalance());
     }
 }
